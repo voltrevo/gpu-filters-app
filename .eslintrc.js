8e26504d452e2e0ce633012bc3ad6e782dc76268
Andrew Morris <voltrevo@gmail.com>
2016-09-05 10:55:02 +1000

    eslint changes

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,16 @@
 module.exports = {
-    "extends": "airbnb",
-    "plugins": [
-        "react",
-        "jsx-a11y",
-        "import"
-    ]
+  extends: 'airbnb-base',
+  env: {
+    browser: true,
+  },
+  plugins: [
+    'react',
+    'jsx-a11y',
+    'import',
+  ],
+  rules: {
+    strict: 0,
+    'func-names': 0,
+    'space-before-function-paren': 0,
+  },
 };

a47f60adaf473dd5669d7b763aa27dc2e954573e
Andrew Morris <voltrevo@gmail.com>
2016-09-05 10:56:30 +1000

    paint some gradients

--- index.js
+++ index.js
@@ -1,20 +1,33 @@
 'use strict';
 
-window.addEventListener('load', () => {
-  navigator.mediaDevices.getUserMedia({video: true}).then(stream => {
-    document.body.style.backgroundColor = '#000';
-    document.body.style.height = '100vh';
-    document.body.style.overflow = 'hidden';
+const GPU = require('./GPU.js');
 
-    const video = document.createElement('video');
-    video.srcObject = stream;
-    video.play();
-    document.body.appendChild(video);
-    video.style.position = 'absolute';
-    video.style.left = '0px';
-    video.style.top = '0px';
-    video.style.width = '100vw';
-    video.style.height = '100vh';
-    video.style.objectFit = 'contain';
-  });
+const gpu = new GPU();
+
+window.addEventListener('load', () => {
+  // navigator.mediaDevices.getUserMedia({video: true}).then(stream => {
+  //   document.body.style.backgroundColor = '#000';
+  //   document.body.style.height = '100vh';
+  //   document.body.style.overflow = 'hidden';
+
+  //   const video = document.createElement('video');
+  //   video.srcObject = stream;
+  //   video.play();
+  //   document.body.appendChild(video);
+  //   video.style.position = 'absolute';
+  //   video.style.left = '0px';
+  //   video.style.top = '0px';
+  //   video.style.width = '100vw';
+  //   video.style.height = '100vh';
+  //   video.style.objectFit = 'contain';
+  // });
+
+  const render = gpu.createKernel(function() {
+    this.color(0, (this.thread.x % 256) / 255, (this.thread.y % 256) / 255, 1);
+  }).dimensions([1280, 720]).graphical(true);
+
+  render();
+
+  const canvas = render.getCanvas();
+  document.body.appendChild(canvas);
 });
